728ed8a3ba877c4bd063feae9de90adab3834a65
“Ivy <ivys725@gmail.com>
2018-06-14 21:30:51 +0200

    payment

---
+++ src/pages/payment-methods/payment-methods.ts
@@ -0,0 +1,111 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { PortfolioPage } from '../portfolio/portfolio';
+import { TabsPage } from '../tabs/tabs';
+
+declare var Stripe;
+
+@Component({
+  selector: 'page-payment-methods',
+  templateUrl: 'payment-methods.html',
+})
+export class PaymentMethodsPage {
+
+  stripe = Stripe('pk_test_9xDCoJstNY3XTH470KJmBNzU');
+  card: any;
+  card_holder: string;
+  amount: number;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public http: Http,
+    private alertCtrl: AlertController) {
+  }
+
+  ionViewDidLoad() {
+    this.setupStripe();
+  }
+
+  stripeTokenHandler(token) {
+    this.http
+      .post("http://localhost:3000/payment", {
+        card_holder: this.card_holder,
+        payment_token: token.id,
+        amount: this.amount,
+        //user_id: localStorage.getItem("TOKEN")
+      })
+
+      .subscribe(
+        result => {
+          console.log(result);
+        },
+
+        error => {
+          console.log(error);
+        });
+  }
+
+  setupStripe() {
+    let elements = this.stripe.elements();
+    var style = {
+      base: {
+        color: '#32325d',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+          color: '#aab7c4'
+        }
+      },
+      invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a'
+      }
+    };
+
+    this.card = elements.create('card', { style: style });
+
+    this.card.mount('#card-element');
+
+    this.card.addEventListener('change', event => {
+      var displayError = document.getElementById('card-errors');
+      if (event.error) {
+        displayError.textContent = event.error.message;
+      } else {
+        displayError.textContent = '';
+      }
+    });
+
+    var form = document.getElementById('payment-form');
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+
+      // this.stripe.createToken(this.card) this.stripe.createSource(this.card)
+      this.stripe.createToken(this.card)
+        .then(result => {
+          if (result.error) {
+            var errorElement = document.getElementById('card-errors');
+            errorElement.textContent = result.error.message;
+          } else {
+            console.log(result.token);
+            this.stripeTokenHandler(result.token);
+            this.navCtrl.setRoot(PortfolioPage);
+            this.donationSuccessful();
+          }
+        })
+    });
+  }
+
+  donationSuccessful() {
+    let alert = this.alertCtrl.create({
+      title: 'Donation Successful',
+      subTitle: 'Thank you for donating!',
+      buttons: ['Ok']
+    });
+    console.log('Donate clicked');
+
+    alert.present();
+  }
+}
