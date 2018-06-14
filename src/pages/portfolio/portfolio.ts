8a4296b09d4a930d4a8a6af3276b7fcc8ce8477d
Amanda Ngo <amanda.mai.ngo@gmail.com>
2018-06-15 00:30:05 +0200

    added donations and updated UI

--- src/pages/portfolio/portfolio.ts
+++ src/pages/portfolio/portfolio.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @IonicPage()
 @Component({
@@ -8,6 +9,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PortfolioPage {
 
+  userDonations: Array<any> = [];
+  sum: number;
+
   @ViewChild('doughnutCanvas') doughnutCanvas;
 
 
@@ -15,11 +19,42 @@ export class PortfolioPage {
 
   //2. when data is passed in, change labels and data of the graph dynamically
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
    
+  }
+
+  showDonations() {
+    this.http.get("http://localhost:3000/donations?jwt=" + localStorage.getItem("Token"),{
+    })
+    .subscribe(
+      result => {
+        this.userDonations = result.json();
+        this.sum = 0;
+        var allDonationAmounts: Array<number> = [];
+       
+        for (var i = 0; i < this.userDonations.length; i++) {
+          var donationAmount = this.userDonations[i].amount; 
+          allDonationAmounts.push(donationAmount);
+        }
+
+        for (var i = 0; i < allDonationAmounts.length; i++) {
+          this.sum += allDonationAmounts[i];
+        }
+
+        console.log(this.sum);
+        return this.sum;
+
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad PortfolioPage")
+    this.showDonations();
 }
+
 }
+
